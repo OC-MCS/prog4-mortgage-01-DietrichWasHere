4976a03b227fef84cd86a71fc56424a0b5370fbb
DVersaw <dietrich.versaw@eagles.oc.edu>
2019-02-16 18:53:04 -0600

    Complete header file, started mortgage.cpp

--- Prog4-Mortgage/Mortgage.cpp
+++ Prog4-Mortgage/Mortgage.cpp
@@ -1,1 +1,21 @@
 // implementation file for Mortgage class
+#include "Mortgage.h"
+
+// set total loan value to nLoan
+void Mortage::setLoan(float nLoan)
+{
+	loan = nLoan;
+}
+
+// set rate value to nRate
+void Mortage::setRate(float nRate)
+{
+	rate = nRate;
+}
+
+// set year value to nYear
+void Mortage::setYears(int nYear)
+{
+	years = nYear;
+}
+
--- Prog4-Mortgage/Mortgage.h
+++ Prog4-Mortgage/Mortgage.h
@@ -1,3 +1,17 @@
 #pragma once
 // header file for Mortgage class
-
+// hold info, functions for mortgage calculations
+class Mortage
+{
+	private:
+		float loan;
+		float rate;
+		int years;
+	public:
+		Mortage();
+		void setLoan(float);
+		void setRate(float);
+		void setYears(int);
+		float getMonthlyPayment();
+		float getTotalMortagePayment();
+};
